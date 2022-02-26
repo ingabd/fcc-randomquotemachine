14724d51f66d294b84b5b00f6a0af22282d670ee
Abdulrachman Hasan <hasanabdulrachman@gmail.com>
2022-02-26 14:27:57 +0700

    Add useEffect fetch quotes -> local state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [quotes, setQuotes] = useState([])
+
+  const url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => {
+        if (res.ok) return res.json()
+        else return Promise.reject(res)
+      })
+      .then(({quotes}) => {
+        setQuotes(quotes)
+      })
+      .catch(err => {
+      console.log(err)
+      })
+  }, [])
+
+  const getQuote = () => {
+    return quotes[Math.floor(Math.random() * quotes.length)]
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +41,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <h3>{JSON.stringify(getQuote())}</h3>
     </div>
   );
 }
