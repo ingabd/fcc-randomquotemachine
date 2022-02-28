d67d5df4553385d771884c9255d3d91ca97bb9c0
Abdulrachman Hasan <hasanabdulrachman@gmail.com>
2022-02-28 11:07:18 +0700

    Add random bg via react, CDN bootstrap, init css

--- src/App.js
+++ src/App.js
@@ -7,6 +7,20 @@ function App() {
   const [quote, setQuote] = useState({})
 
   const url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+  const colors = [
+    '#16a085',
+    '#27ae60',
+    '#2c3e50',
+    '#f39c12',
+    '#e74c3c',
+    '#9b59b6',
+    '#FB6964',
+    '#342224',
+    '#472E32',
+    '#BDBB99',
+    '#77B1A9',
+    '#73A857'
+  ]
 
   useEffect(() => {
     fetch(url)
@@ -27,16 +41,22 @@ function App() {
     setQuote(quotes[Math.floor(Math.random() * quotes.length)])
   }
 
+  const randomBg = () => {
+    return {
+      "background-color": colors[Math.floor(Math.random() * colors.length)]
+    }
+  }
+
   return (
-    <div id="quote-box" className="box">
+    <div id="quote-box" className="box" style={randomBg()}>
       <div id="text">
         <h3>{quote.quote}</h3>
       </div>
       <div id="author">
         <h4>{quote.author}</h4>
       </div>
-      <button id="new-quote" onClick={getQuote}>New Quote</button>
-      <a id="tweet-quote" onClick={getQuote}>tweet quote</a>
+      <button id="new-quote" className="btn btn-primary" onClick={getQuote}>New Quote</button>
+      <a id="tweet-quote" href="twitter.com/intent/tweet">tweet quote</a>
     </div>
   );
 }
